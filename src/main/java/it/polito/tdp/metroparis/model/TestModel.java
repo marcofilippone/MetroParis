aea870a51427439ab8fa9752b75a86b033288c88
Marco Filippone <s257970@studenti.polito.it>
2021-05-03 11:08:34 +0200

    calcolo cammini minimi completato

---
+++ src/main/java/it/polito/tdp/metroparis/model/TestModel.java
@@ -0,0 +1,27 @@
+package it.polito.tdp.metroparis.model;
+
+import java.util.List;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		Model m = new Model();
+		
+		m.creaGrafo();
+		
+		Fermata p = m.trovaFermata("La Fourche");
+		if(p==null) {
+			
+		} else{
+			List<Fermata> raggiungibili = m.fermateRaggiungibili(p);
+			//System.out.println(raggiungibili);
+		}
+		
+		
+		Fermata a = m.trovaFermata("Temple");
+		
+		List<Fermata> percorso = m.trovaCammino(p, a);
+		System.out.println(percorso);
+	}
+
+}
